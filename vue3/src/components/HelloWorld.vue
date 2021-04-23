59c092c35a83724f9186ff89fe5c9378f92b2f8e
zhangxuchao <2212362272@qq.com>
2021-04-23 10:36:33 +0800

    Update HelloWorld.vue

--- vue3/src/components/HelloWorld.vue
+++ vue3/src/components/HelloWorld.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h2>
+      {{state.loginState}}
+    </h2>
+    <h1 @click="changeVal">{{ msg }}</h1>
   </div>
 </template>
-
 <script>
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
+import { useStore } from 'vuex'
 import { LOGIN } from '../http/api'
 export default {
   name: 'HelloWorld',
@@ -13,16 +16,32 @@ export default {
     msg: String
   },
   setup () {
+    // 定义响应式变量
+    const state = reactive({
+      loginState: 1
+    })
+    // 使用vuex
+    const store = useStore()
+    // 生命周期函数
     onMounted (() => {
+      state.loginState = store.state.loginState
       LOGIN({username: 'zhangxuchao', password: '123456'}).then(res => {
         console.log(res)
       })
     })
+    // 定义方法
+    const changeVal = () => {
+      store.commit('changeLoginState')
+      state.loginState = store.state.loginState
+    }
+    // 导出，view中使用
+    return {
+      state,
+      changeVal
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
